a875836484aa06e8e0bf9fec9dd229740bfcfde6
epe105 <19913916+epe105@users.noreply.github.com>
2024-01-31 10:34:53 -0500

    updated tf

--- main.tf
+++ main.tf
@@ -7,12 +7,14 @@ provider "aws" {
 
 # Filter out local zones, which are not currently supported 
 # with managed node groups
-data "aws_availability_zones" "available" {
-  filter {
-    name   = "opt-in-status"
-    values = ["opt-in-not-required"]
-  }
-}
+
+#data "aws_availability_zones" "available" {
+  #filter {
+  #  name   = "opt-in-status"
+  #  values = ["opt-in-not-required"]
+  #}
+  
+#}
 
 locals {
   cluster_name = "education-eks-${random_string.suffix.result}"
@@ -30,7 +32,8 @@ module "vpc" {
   name = "education-vpc"
 
   cidr = "10.0.0.0/16"
-  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+  #azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+  azs             = ["us-east-1a", "us-east-1b", "eu-east-1c"]
 
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
